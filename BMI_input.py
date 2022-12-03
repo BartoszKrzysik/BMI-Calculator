10de8676e96643587f7445658207fe22caad482b
BartoszKrzysik <116747979+BartoszKrzysik@users.noreply.github.com>
2022-12-03 19:03:06 +0100

    Update BMI_input.py
    Opis procedur

--- BMI_input.py
+++ BMI_input.py
@@ -1,34 +1,42 @@
 List = []
-
+#lista z osobami
 no_osob = int(input("Wpisz liczbe osob"))
 a = float(2)
-
+#no osbo, liczba osob i wykonan w petli
 for i in range(no_osob):
     wzrost = "Wzrost="
     waga = "Waga="
     cale = "Cale="
     funty = "Funty="
     bmi = "BMI="
+    #zmienne do zapisu w listach
     metrical_input = input(
         "Czy chcesz korzystac z jednostek metrycznych? yes/no")
+    #z jakich jednostek korzystać
     if metrical_input.lower() == "yes":
         name = input("Wpisz nazwe/imie osoby (Wazne aby sie nie duplikowala z "
                      "poprzednimi)")
         imie = name
         name = []
         name.append(imie)
+        #name jako nazwa listy + imię w arg0
         height = float(input("Wpisz wzrost w centymetrach"))
         height = height / 100
         wzrost = wzrost + str(height)
+        #wzrost w m, można zapisać jako czysta liczba
         name.append(wzrost)
         weight = float(input("Wpisz wage w kilogramach"))
         waga = waga + str(weight)
+        #analogicznie jak wyżej
         name.append(waga)
         List.append(name)
         BMI = weight / height ** a
+        #wzór na BMI
         bmi = bmi + str(BMI)
         name.append(bmi)
+        #analogicznie do wzrostu
     elif metrical_input.lower() == "no":
+        #przy jednostkach imperialnych analogicznie do metrycznych
         name = input("Wpisz nazwe/imie osoby (Wazne aby sie nie duplikowala z "
                      "poprzednimi)")
         imie = name
@@ -54,7 +62,7 @@ for i in range(no_osob):
         bmi = bmi + str(BMI)
         name.append(bmi)
         List.append(name)
-print(List)
 import json
 with open('Wyniki.txt', 'w') as f:
     f.write(json.dumps(List))
+#zapisanie danych w txt
