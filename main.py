19154864aa9dad8e1c35c2a1e4245ac500041fc0
GKawcki <grzegorz.kavecki@gmail.com>
2022-12-04 17:55:19 +0100

    gittttt

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from BMI_input import wsadzanie
 from bmi_chart import chart
-wsadzanie()
-chart()
+from BMI_text import text
+
+wybor = input("Czy chcesz wprowadzic swoj plik .cvs? yes/no")
+if wybor.lower() == "yes":
+    path = input("Podaj sciezke do pliku")
+    text(path)
+    chart()
+elif wybor.lower() == "no":
+    print("wprowadz dane recznie: ")
+    wsadzanie()
+    chart()
+
