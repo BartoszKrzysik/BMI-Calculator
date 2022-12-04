63bedbabf2c91d2fb2723831f41c7894e1e4169b
jkochana <116748315+jkochana@users.noreply.github.com>
2022-12-04 13:31:05 +0100

    Add files via upload

---
+++ bmi_chart.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+bmi = pd.read_csv('arkuszwyniki.csv')
+print(bmi)
+ax = sns.barplot(x='Imie', y='BMI', data=bmi, order=bmi.sort_values('BMI').Imie, palette="coolwarm")
+for i in ax.containers:
+    ax.bar_label(i,)
+plt.show()
