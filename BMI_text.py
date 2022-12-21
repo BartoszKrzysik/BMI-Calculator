da73f90bb009b463c16c73eeafae34f285c1ec41
hubertgolda <116748013+hubertgolda@users.noreply.github.com>
2022-12-21 15:47:31 +0100

    Update BMI_text.py

--- BMI_text.py
+++ BMI_text.py
@@ -1,12 +1,11 @@
 def text(filepath):
-    import csv
+    import csv #importuje bibliotekę do obsługi plików CSV
 
-    # filepath = "pomiary2.csv"
 
     with open(filepath) as csvfile:
         with open("arkuszwyniki.csv", mode='w') as f:
-            reader = csv.reader(csvfile, delimiter=';')
-            writer = csv.writer(f, delimiter=',', lineterminator='\n')
+            reader = csv.reader(csvfile, delimiter=';') #komenda do zczytywania
+            writer = csv.writer(f, delimiter=',', lineterminator='\n') #komenda do zapisywania
             wiersze = list(reader)
             naglowek = wiersze.pop(0)  # pomiń naglowek
             naglowek.append("BMI")
@@ -14,6 +13,6 @@ def text(filepath):
             for wiersz in wiersze:
                 wzrost = float(wiersz[3]) / 100
                 waga = float(wiersz[4])
-                BMI = round(waga / (wzrost * wzrost), 2)
+                BMI = round(waga / (wzrost * wzrost), 2) #obliczanie BMI
                 wiersz.append(str(BMI))
                 writer.writerow(wiersz)
